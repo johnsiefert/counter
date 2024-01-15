4ab43c1177e332cfe21f8d8a4cfdb7182bbde3f9
John <johnsief12@gmail.com>
2024-01-15 18:25:03 -0500

    finished counter project increasing/decreasing proj

---
+++ app.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
+let count = 0
+
+buttons.forEach(function (btn) {
+btn.addEventListener('click',function(e){
+    const styles = e.currentTarget.classList;
+if(styles.contains('decrease')){
+    count--
+    value.style.color = 'red'
+}else if(styles.contains('increase')){
+    count++
+}else {
+    count = 0
+}
+if(count > 0){
+value.style.color = 'green'
+}
+if(count < 0){
+    value.style.color ='red'
+}
+if(count === 0){
+    value.style.color = 'black'
+}
+ value.textContent = count;
+})
+})
+
+
+
+
+// decrease.addEventListener('click', decreaseBtn);
+// reset.addEventListener('click', resetBtn);
